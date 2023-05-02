1afe877a566dbed95200dcc038f9699a8ab2b95f
Anastasiya Shirochina <shirochina16@gmail.com>
2023-05-02 19:50:15 +0300

    change name of multiply method

--- src/main/java/by/itacademy/shirochina/anastasiya/Calc.java
+++ src/main/java/by/itacademy/shirochina/anastasiya/Calc.java
@@ -8,7 +8,7 @@ public class Calc {
     public static int subtract(int a, int b) {
         return a - b;
     }
-    public static int multiplicate(int a, int b) {
+    public static int multiply(int a, int b) {
         return a * b;
     }
     public static int divide(int a, int b) {
