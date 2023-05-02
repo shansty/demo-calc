9a0a026e5e4de88f5c0a11c2be4a0e2bd80f1e9e
Anastasiya Shirochina <shirochina16@gmail.com>
2023-05-02 19:43:26 +0300

    add 4 methods calc

--- src/main/java/by/itacademy/shirochina/anastasiya/Calc.java
+++ src/main/java/by/itacademy/shirochina/anastasiya/Calc.java
@@ -4,4 +4,14 @@ public class Calc {
     public static int sum(int a, int b) {
         return a + b;
     }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+    public static int multiplicate(int a, int b) {
+        return a * b;
+    }
+    public static int divide(int a, int b) {
+        return a / b;
+    }
 }
